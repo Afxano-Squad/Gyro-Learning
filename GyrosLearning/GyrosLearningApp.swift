420180880163af9fa217f1e39e32513e207634fd
YurikoAishinselo123 <aishinseloyuriko124@gmail.com>
2024-10-21 11:26:27 +0700

    Initial Commit

---
+++ GyrosLearning/GyrosLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  GyrosLearningApp.swift
+//  GyrosLearning
+//
+//  Created by Yuriko AIshinselo on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GyrosLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GyrosLearningTests/GyrosLearningTests.swift
@@ -0,0 +1,17 @@
+//
+//  GyrosLearningTests.swift
+//  GyrosLearningTests
+//
+//  Created by Yuriko AIshinselo on 21/10/24.
+//
+
+import Testing
+@testable import GyrosLearning
+
+struct GyrosLearningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
